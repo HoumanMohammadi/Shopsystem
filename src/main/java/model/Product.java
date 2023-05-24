f838e8a7239726242bcf8e7389e880568470d908
Houman Mohammadi <houmane.el@gmail.com>
2023-05-24 10:28:42 +0200

    first commit

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.Objects;
+
+public class Product {
+
+    //1.Atributes= properties of the class
+
+    private String id;
+    private String name;
+
+    //2.constructor
+
+
+    public Product(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //3.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
